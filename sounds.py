1b8c751349ea9431989a573c59a01bd86f158e24
L4zi <luffygacc@gmail.com>
2026-03-15 15:11:08 +0500

    add more sounds

--- sounds.py
+++ sounds.py
@@ -4,4 +4,11 @@ SOUNDS = {
     "the scream": "https://www.myinstants.com/media/sounds/chicken-screaming-on-a-tree.mp3",
     "blyat": "https://www.myinstants.com/media/sounds/blyat_9T1fo5H.mp3",
     "please lower my cortisol": "https://www.myinstants.com/media/sounds/low-cortisol-song.mp3",
+    "brother eww": "https://www.myinstants.com/media/sounds/brother-ewwwwwww.mp3",
+    "my granny ;-;": "https://www.myinstants.com/media/sounds/rip-my-granny-she-got-hit-by-a-bazooka.mp3",
+    "dexter": "https://www.myinstants.com/media/sounds/dexter-meme.mp3",
+    "prowler": "https://www.myinstants.com/media/sounds/prowler-sound-effect_6bXErot.mp3",
+    "indiansong": "https://www.myinstants.com/media/sounds/indian-song.mp3",
+    "my mommy said no more": "https://www.myinstants.com/media/sounds/my-mommy-said-no-more-skibidi-toilet.mp3",
+    "kim jong un": "https://www.myinstants.com/media/sounds/kim-jong-un-is-master-of-g00n.mp3",
 }
